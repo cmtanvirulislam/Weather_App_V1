a0631f6b9663980eeacc8daf8b692d8a55072470
Tanvir <engr.cmtanvir@gmail.com>
2021-04-13 15:32:40 +0600

    Appbar Created

---
+++ lib/weather.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tanvir Weather App'),
+      ),
+    );
+  }
+}
